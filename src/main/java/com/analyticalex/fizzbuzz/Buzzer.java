2edecf7860d00791d4a1892d729dd3025463b5c9
Alex French <alexandra.j.french@gmail.com>
2017-02-03 13:56:26 -0800

    Setting up README and FizzBuzz

---
+++ src/main/java/com/analyticalex/fizzbuzz/Buzzer.java
@@ -0,0 +1,10 @@
+package main.java.com.analyticalex.fizzbuzz;
+
+public class Buzzer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/analyticalex/fizzbuzz/BuzzerTest.java
@@ -0,0 +1,14 @@
+package test.java.com.analyticalex.fizzbuzz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BuzzerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
